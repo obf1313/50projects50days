1761ae65f2b127494b51c0d937f1e8f020415df5
obf1313 <oubaifan@163.com>
2023-05-09 17:48:11 +0800

    feat: 跑马灯

---
+++ find-cascader/index.js
@@ -0,0 +1,354 @@
+const tree = [
+  {
+    code: '29338469de50440eba1a1ed36cc5e15a',
+    name: '三级',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: true,
+    tagTree: [
+      {
+        code: '0e7da99dacb64ab498adbded695df372',
+        name: '3级',
+        dirCode: '29338469de50440eba1a1ed36cc5e15a',
+        tagDir: true,
+        tagTree: [
+          {
+            code: 'd47bce853cdb412f82559d28c771ad30',
+            name: '4级',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: true,
+            tagTree: [],
+          },
+          {
+            code: 'c1922003bf3842e48eb79a91570e257f',
+            name: '思考世界的孩子',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '7f309ee0042143ac8278cdb18cec3e3a',
+            name: '猜猜我有多爱你',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '543eadafbd774d66aaabe753e6441020',
+            name: '生态关注',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: 'f21256a5da9a4f4a8833794a5f44d3fa',
+            name: '给孩子的成长课',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '8e5d0a84fb654bc397e8a417afab01e3',
+            name: '故宫里的大怪兽',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '03e67487158c4f6f9667b56f10ab01e5',
+            name: '远行译丛',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '0e6ee9ea534945afb8d6725905a9aa38',
+            name: '讲了100万次的故事',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+          {
+            code: '93e0bfb7849a4430bca86f43b85e33b5',
+            name: '孩子的诗园',
+            dirCode: '0e7da99dacb64ab498adbded695df372',
+            tagDir: false,
+            tagTree: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    code: '2dd2625148744dd78d2091235922c70e',
+    name: '感官认知',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '887ad33cb3c54999a91b2a0d9f77d46e',
+    name: '知识启蒙',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'f8fc649bf58b4053a9629abf5b759880',
+    name: '社会认知',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '901ee69fd2864408ba7bc5064a7b6ae4',
+    name: '智力开发',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '92b25ef22a7d4654aa7b5a545687a898',
+    name: '涂色绘画',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'c1f42614bc3e4117b00b108f37cbee65',
+    name: '创意游戏',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'c5f33d70fbd4409bb23d84b2ecdb0aed',
+    name: '智力手工',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '66b59ab180084cae92e9ea96d2f6da42',
+    name: '少儿英语',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'cac6a153c97e426a9dc0cf37d116fc80',
+    name: '睡前故事',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'f5cdbbf99494449cbfc09c029afc5f69',
+    name: '品格故事',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '93f1e3807f334259bad00794121feeb2',
+    name: '幼儿故事',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '4e6dda4d19fb414aada9ca1b8458c8e5',
+    name: '大奖绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'e02863c9c71a42928695230a446cb0a6',
+    name: '创意想象绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '47ce4059808e4e4bb0dd9d1efd056e76',
+    name: '情感交流绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '7788a55ba96145ea8fcb6568dc2deac0',
+    name: '情商品格绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '8522cdd84f70446fb15c21b6b1f20f81',
+    name: '行为习惯绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '6bd2657d534342bb8bccdacc832160ab',
+    name: '传统原创绘本',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '1be85fe7f8e74d98836f621b1a592d5c',
+    name: '动漫品牌',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '25db7dd6287a4115bc9166d2dca62379',
+    name: '漫画读物',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '3f73d54070e7485b9b9da28e29269af4',
+    name: '国漫童年',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '4636ba0e1b674222a46947b4842eb78d',
+    name: '少年读国学',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '41cbbce278194279ae7726a7367e3b1c',
+    name: '传统故事',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '85c1a7b12dde446b9557d3b667fb1c76',
+    name: '成语诗文',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '35b5be2aa2d94cbf87a1de789d1b0594',
+    name: '文化趣谈',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '08fc9edecaeb478aa7ff1c19b4b1b0fa',
+    name: '尤斯伯恩科普书',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '6f1238d9d482466baafb3043491919d9',
+    name: 'DK儿童百科全书',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '08dfca66c5284568923748c7fb3179a3',
+    name: '自然百科',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '2747a06d279041f9afc42fa70a737908',
+    name: '人文百科',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '4568bcd4b38e409c847b4fb6662aa414',
+    name: '科学百科',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '5a8822da8ec941b6920ced816605d689',
+    name: '儿童名家',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '1f3d0f1f49f94cfbae59b39431a4f31b',
+    name: '经典文学',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'f5312f6e9da3401d8d5f95035d71cff3',
+    name: '幻想冒险',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '5b9548af99244094acdf3aa1c04ff232',
+    name: '校园成长',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: 'f2cec7c00df74e8d8beb41d0de1abd3b',
+    name: '注音读物',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+  {
+    code: '57caf00860314c60813381479388d8bc',
+    name: '课外阅读',
+    dirCode: '43c916fdd08048d78b6a183c975cfee6',
+    tagDir: false,
+    tagTree: null,
+  },
+]
+
+const targetCode = '7f309ee0042143ac8278cdb18cec3e3a'
+
+const findCascader = (tree, targetCode) => {
+  let returnList = []
+  // 遍历树生成树节点数组
+  const initTreeNode = (tree, lastCodeList) => {
+    tree.forEach(item => {
+      // 如果之前有 codeList
+      let codeList = []
+      if (lastCodeList) {
+        codeList = [...lastCodeList]
+      }
+      codeList.push(item.code)
+      if (item.tagTree) {
+        initTreeNode(item.tagTree, codeList)
+      } else {
+        if (item.code === targetCode) {
+          returnList = codeList
+        }
+      }
+    })
+  }
+  initTreeNode(tree)
+  console.log('returnList', returnList)
+}
+
+findCascader(tree, targetCode)
